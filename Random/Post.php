1044a9c5fd301c1353377008e97e7efbb5b988d1
Bmc9529 <bmc9529@stacmail.net>
2022-05-04 10:30:53 +1200

    proto work

--- Random/Post.php
+++ Random/Post.php
@@ -1,9 +1,14 @@
 <?php
-$student = $_POST['student'];
+session_start();
+include 'dbconnect.php';
+ ?>
+
+<?php
+$student = $_SESSION['students'];
 if (is_array($student)) {
   foreach ($student as $studentID) {
     echo "$studentID <br>";
   }
 }
-
  ?>
+<a href="Postto.php">back</a>
--- Random/Postto.php
+++ Random/Postto.php
@@ -1,24 +1,64 @@
-<form action="Post.php" method="post">
-  <table>
-    <thead>
-      <tr>
-        <th scope="col" class="w-overide-0"></th>
-        <th scope="col">Student Name:</th>
-        <th scope="col" class="text-center">Year:</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row" class="pt-0 pb-0"><input class="form-check-input display-7 m-0" type="checkbox" name='student[]' value='5' id="flexCheckChecked" checked></th>
-        <td>Jacob Horrey</td>
-        <td class="text-center">11</td>
-      </tr>
-      <tr>
-        <th scope="row" class="pt-0 pb-0"><input class="form-check-input display-7 m-0" type="checkbox" name='student[]' value='2' id="flexCheckChecked" checked></th>
-        <td>Bumsen Burner</td>
-        <td class="text-center">12</td>
-      </tr>
-    </tbody>
-  </table>
-  <button type="submit">Submit</button>
-</form>
+<?php
+session_start();
+include 'dbconnect.php';
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>postto</title>
+     <!-- CSS goes up here for bootrap -->
+     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+     <link rel="stylesheet" href="custom.css">
+     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+     <script type="text/javascript">
+     // This function takes the studentID entered and gets their details
+     // The studentDetails.php page is then updated
+     function selectstudent(studentID) {
+
+       var xmlhttp = new XMLHttpRequest();
+       xmlhttp.onreadystatechange = function() {
+       if (this.readyState == 4 && this.status == 200) {
+         document.getElementById("studenttable").innerHTML = this.responseText;
+       }
+     };
+     xmlhttp.open("GET","studentDetails.php?studentID="+studentID,true);
+     xmlhttp.send();
+     }
+     </script>
+     <script type="text/javascript">
+        $(function() {
+          $( "#term" ).autocomplete({source: 'autocomplete.php',});});
+      </script>
+   </head>
+   <body>
+     <div class="container">
+       <div class="row">
+         <div class="col">
+           <form class="input-group input-group-lg mt-3" action="Postto.php" method="post">
+             <input class="form-control input-lg" type="text" autocomplete="off" placeholder="Search Here..." name="search" id="search" value="">
+             <button class="btn btn-primary" type="submit">Submit</button>
+           </form>
+         </div>
+       </div>
+     </div>
+     <?php
+     if (isset($_POST['search'])) {
+       $studentID = $_POST['search'];
+       if ($studentID == -1) {
+         session_unset();
+       } else {
+         if(isset($_SESSION['students'])){
+           array_push($_SESSION['students'],$studentID);
+         } else {
+           $_SESSION['students']=[$studentID];
+         }
+       }
+     }
+      ?>
+
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   </body>
+ </html>
